107012e3563017eb1fac566d7969f423b4d7a50f
Francis Rafal <francis.rafal@gmail.com>
2022-11-01 15:32:03 +0100

    feat: Comment Architecture

--- ft_printf.c
+++ ft_printf.c
@@ -6,15 +6,23 @@
 /*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 12:59:34 by frafal            #+#    #+#             */
-/*   Updated: 2022/11/01 14:50:22 by frafal           ###   ########.fr       */
+/*   Updated: 2022/11/01 15:31:33 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft/libft.h"
 
-int	ft_printf(const char *s, ...)
+int	ft_printf(const char *format, ...)
 {
-	(void)s;
+	(void)format;
+	// Allowed External Functions:
+	// malloc, free, write, va_start, va_arg, va_copy, va_end 
+	// Parse Variable Arguments
+	// Parse Format String
+	// Replace Format String with Variable Arguments
+	// Print to Stdout
+	// Count the Number of characters printed excluding null byte and return
+	// Error should return negative value
 	return (0);
 }
---
+++ ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/01 14:56:21 by frafal            #+#    #+#             */
+/*   Updated: 2022/11/01 15:01:40 by frafal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_printf(const char *format, ...);
+
+#endif
